1eb6b9af1de931a0e252db099cc3d9ccafeeb7d6
parijatSaha97 <samrat.saha015@gmail.com>
2023-12-03 19:53:55 +0530

    frist commit

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+// $host_name = 'localhost';
+// $usr_name = 'root';
+// $password = '';
+// $db_name = 'clinical_db_test';
+
+// $conn = mysqli_connect($host_name,$usr_name,$password,$db_name);
+$conn = mysqli_connect('localhost','root','','clinical_db_test');
+ ?>
